4291a436d047161285bded31726b1ce77f1cdc54
handev-id <mparhanmaulana17@gmail.com>
2025-11-26 15:53:40 +0700

    finish fcm

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/idnoffice/app/test/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.idnoffice.app.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.idnoffice.app.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String APP_URL = "https://idnoffice.videaclass.com/app";
+  // Field from default config.
+  public static final String FCM_API_URL = "https://videaclass.com/api/v1/fcm-token";
+  // Field from default config.
+  public static final String TENANT_CODE = "35F64AC7";
+}
